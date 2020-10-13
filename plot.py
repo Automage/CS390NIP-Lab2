d77b8d9eab73cb48567fee128c3829035aa88f3c
Automage <pranav92000@gmail.com>
2020-10-13 08:00:02 +0400

    fini

--- plot.py
+++ plot.py
@@ -1,16 +1,16 @@
 import matplotlib.pyplot as plt
 datasets = ['minst-d', 'minst-f', 'cifar-10', 'cifar-100-c', 'cifar-100-f']
-accuracy = []
+accuracy = [93.43, 76.59, 0, 0, 0]
 
-with open("cnn_data.txt") as file:
-    content = file.readlines()
-    for line in content:
-        if len(line.rstrip()) != 0:
-            accuracy.append(float(line.rstrip()))
+# with open("cnn_data.txt") as file:
+#     content = file.readlines()
+#     for line in content:
+#         if len(line.rstrip()) != 0:
+#             accuracy.append(float(line.rstrip()))
 
 print(accuracy)
 plt.bar(datasets, accuracy)
 plt.xlabel("Dataset")
 plt.ylabel("Accuracy")
-plt.title("CNN classification accuracy on different datasets")
+plt.title("ANN classification accuracy on different datasets")
 plt.show()
